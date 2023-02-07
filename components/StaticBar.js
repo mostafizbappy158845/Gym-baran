c8af47cd7eb1655da55bdf498ad4d67dcfe30d56
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-02-07 13:27:25 +0600

    static-bar pic-position

--- components/StaticBar.js
+++ components/StaticBar.js
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import dancerMotion from '../public/assets/dancerMotion.png'
+import pose1 from '../public/assets/pose1.png'
+import pose2 from '../public/assets/pose2.png'
+import pose3 from '../public/assets/pose3.png'
+import head1 from '../public/assets/head1.png'
 
 const StaticBar = () => {
     return (
         <section className=" mx-10 px-2 ">
             <div className=" max-w-[1100px] rounded-[30px] bg-gradient-to-r from-[#6462F0] to-[#9190E9] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center justify-items-center gap-[40px] p-7">
                 
-                <div className="flex relative  flex-col-2   justify-items-center gap-4 ">
+                <div className="flex relative flex-col-2   justify-items-center gap-4 ">
                     <div className=" w-[100px] h-[100px] rounded-[20px] bg-[#A09EEE]">
-                        <div >
-                        <Image className="absolute bottom-5" src={dancerMotion} alt='dancerMotion'></Image>
-                        </div>
+                    <Image className="absolute top-[19%] left-[9%]" src={pose1} alt='dancerMotion'></Image>
+                        
                     </div>
                     <div className="mr-[32px] text-[16px] text-white font-['Poppins'] leading-7 font-bold">
                         <h1 className=" text-white text-[13px] md:text-[16px] lg:text-[20px] font-['Poppins'] leading-7 font-bold">Get that 11 line<br />in 30 days</h1>
@@ -27,17 +29,18 @@ const StaticBar = () => {
                     </div>
                 </div>
                     
-                <div className="flex flex-col-2  justify-items-center gap-8 ">
-                    <div className="w-[100px] h-[100px] rounded-[20px] bg-white backdrop-blur-[32px] opacity-20">
+                <div className="relative flex flex-col-2  justify-items-center gap-8 ">
+                    <div className="w-[100px] h-[100px] rounded-[20px] bg-[#A09EEE]">
                         {/* <Image className="" src={dancerMotion} alt='dancerMotion'></Image> */}
+                        <Image className="absolute top-[16%] left-[8%]" src={pose2} alt='dancerMotion'></Image>
                     </div>
                     <div className="mr-[30px]">
                         <h1 className=" text-white text-[13px] md:text-[16px] lg:text-[20px] font-['Poppins'] leading-4 sm:leading-6 md:leading-7 font-bold">14 Days<br />sherd challenge</h1>
-                        <div className="flex justify-between items-center opacity-50">
+                        <div className="flex justify-between items-center opacity-20">
                             <p className="mt-4 font-normal text-white text-[16px] tracking-[.01em]  ">Learn more</p>
                            
                             
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mt-4 w-6 h-5 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" mt-4 w-6 h-5 ">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                             </svg>
                             
@@ -47,9 +50,10 @@ const StaticBar = () => {
 
                 </div>
             
-                <div className="flex flex-col-2  justify-items-center gap-8">
-                    <div className="w-[100px] h-[100px] rounded-[20px] bg-white backdrop-blur-[32px] opacity-20">
+                <div className="relative flex flex-col-2  justify-items-center gap-8">
+                    <div className="w-[100px] h-[100px] rounded-[20px] bg-[#A09EEE]">
                         {/* <Image className="" src={dancerMotion} alt='dancerMotion'></Image> */}
+                        <Image className="absolute top-[20%] left-[9%]" src={pose3} alt='dancerMotion'></Image>
                     </div>
                     <div className=" mr-[47px] text-white font-['Poppins'] leading-7 font-bold">
                         <h1 className=" text-white text-[13px] md:text-[16px] lg:text-[20px] font-['Poppins'] leading-7 font-bold">Get flat belly <br/>in 30 days</h1>
